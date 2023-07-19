073cc9a3f4c43ce33b06f1a7310bdfa07ad5bcca
Magay Denis <qwertydenis009@gmail.com>
2023-07-19 03:06:08 +0300

    Задачи 1-4

---
+++ src/Main.java
@@ -0,0 +1,95 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        public static int[] generateRandomArray () {
+            java.util.Random random = new java.util.Random();
+            int[] arr = new int[30];
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = random.nextInt(100_000) + 100_000;
+            }
+            return arr;
+        }
+
+        System.out.println("Задача 1");
+
+        int[] salaryArray = generateRandomArray();
+        int salarySum = 0;
+
+        System.out.println(Arrays.toString(salaryArray));
+
+        for (int salary : salaryArray) {
+            salarySum += salary;
+        }
+
+        System.out.println("Сумма трат за месяц составила " + salarySum + "рублей.");
+    }
+
+    private static void task2() {
+        System.out.println("Задача 2");
+
+        public static int[] generateRandomArray () {
+            java.util.Random random = new java.util.Random();
+            int[] arr = new int[30];
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = random.nextInt(100_000) + 100_000;
+            }
+            return arr;
+        }
+
+        int[] salaryArray = generateRandomArray();
+        int salary = 0;
+        System.out.println(Arrays.toString(salaryArray));
+
+        int minExpenses = salaryArray[0];
+        int maxExpenses = salaryArray[0];
+
+        for (int salary : salaryArray[0]) {
+            if (salary < minExpenses) {
+                minExpenses = salary;
+            }
+
+            if (salary > maxExpenses) {
+                maxExpenses = salary
+            }
+        }
+
+        System.out.println("Минимальная сумма трат за день составила" + minExpenses +
+                "рублей. Максимальная сумма трат за день составила" + maxExpenses + "Рублей.");
+    }
+
+    private static void task3() {
+        System.out.println("Задача 3");
+
+        public static int[] generateRandomArray () {
+            java.util.Random random = new java.util.Random();
+            int[] arr = new int[30];
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = random.nextInt(100_000) + 100_000;
+            }
+            return arr;
+        }
+
+        int[] salaryArray = generateRandomArray();
+        int salarySum = 0;
+
+        System.out.println(Arrays.toString(salaryArray));
+
+        for (int salary : salaryArray) {
+            salarySum += salary;
+        }
+
+        double salaryAverage = (double) salarySum / salaryArray.length;
+
+        System.out.println("Средняя сумма трат за день составила " + salaryAverage + "Рублей.");
+    }
+
+    private static void task4() {
+        System.out.println("Задача 4");
+
+        char reverseFullName = { 'n', 'a', 'v', 'i', ' ', 'v', 'o', 'n', 'a', 'v', 'i',};
+
+        for (int i = reverseFullName.lenght - 1; i >= 0; i--) {
+            System.out.println(reverseFullName[i]);
+        }
